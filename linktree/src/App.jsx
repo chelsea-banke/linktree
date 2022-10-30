5594c4b246497d144fde298112dddcc1c2c3c37f
chelsea-banke <chelseanewhorizon@gmail.com>
2022-10-30 22:02:41 +0100

    cretion and population of list of link objects

--- linktree/src/App.jsx
+++ linktree/src/App.jsx
@@ -4,6 +4,32 @@ import Link from './components/Link/Link.jsx'
 import './App.css'
 
 function App() {
+  const links = [
+    {
+      title: 'zuri Team',
+      id: 'btn__zuri',
+      url: 'https://training.zuri.team/',
+      subtext: ''
+    },
+    {
+      title: 'Zuri Books',
+      id: 'books',
+      url: 'https://books.zuri.team',
+      subtext: ''
+    },
+    {
+      title: 'Python Books',
+      id: 'book__python',
+      url: 'https://books.zuri.team/python-for-beginners?ref_id=<Chelsea Banke>',
+      subtext: ''
+    },
+    {
+      title: 'Background check for coders',
+      id: 'pitch',
+      url: 'https://background.zuri.team',
+      subtext: ''
+    },
+  ]
   const [count, setCount] = useState(0)
 
   return (
